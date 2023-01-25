062c54ca7a1f5c5ffed6f7635dfeb5349153e467
mweaver20 <83778978+mweaver20@users.noreply.github.com>
2023-01-24 17:51:10 -0800

    setting up light and dark theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, dividerClasses, ThemeProvider } from "@mui/material";
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <div className="app"></div>;
+    </ColorModeContext.Provider>
   );
 }
 
