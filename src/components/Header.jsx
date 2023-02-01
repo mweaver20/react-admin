238b3691f609bf935325eb7939b24c4b5936d50f
mweaver20 <83778978+mweaver20@users.noreply.github.com>
2023-01-31 16:07:41 -0800

    more syntax fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import { Typography } from "@mui/material";
+import { Typography, Box, useTheme } from "@mui/material";
 import { tokens } from "../theme";
 
 const Header = ({ title, subtitle}) => {
     const theme = useTheme();
-    const colors = tokens(theme.pallete.mode);
+    const colors = tokens(theme.palette.mode);
     return (
         <Box mb="30px">
             <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{ mb: "5px"}}  >{title}</Typography>
